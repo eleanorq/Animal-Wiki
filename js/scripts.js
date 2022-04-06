411b09c737bb29f15429fc6a1a1ad0c7d399783e
Eleanor Quandt <elquandt@gmail.com>
2022-04-05 19:16:17 -0700

    add branching to js

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  const fish = parseInt(prompt("Which of my fish do you want to learn about?"));
+
+  if (fish >= Mystery Snails) {
+    $('#fish1').show();
+  }
+});
+
+$(document).ready(function() {
+  const fish = parseInt(prompt("Which of my fish do you want to learn about?"));
+
+  if (fish >= Bolivan Rams) {
+    $('#fish2').show();
+  }
+});
+
+$(document).ready(function() {
+  const fish = parseInt(prompt("Which of my fish do you want to learn about?"));
+
+  if (fish >= Bumblebee Goby) {
+    $('#fish3').show();
+  }
+});
+
+$(document).ready(function() {
+  const fish = parseInt(prompt("Which of my fish do you want to learn about?"));
+
+  if (fish >= Corydora Catfish) {
+    $('#fish4').show();
+  }
+});
+
+$(document).ready(function() {
+  const fish = parseInt(prompt("Which of my fish do you want to learn about?"));
+
+  if (fish >= Betta) {
+    $('#fish5').show();
+  }
+});
